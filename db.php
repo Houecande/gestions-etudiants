db561af4d1bd1358f0ba0b8d396cf19b6595cc5f
Armand Blade <armandhouecande9@gmail.com>
2026-04-23 13:57:37 +0100

    Configuration de la connexion réutilisable à la base de données

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$dbname = 'gestion_etudiants';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Configuration de PDO pour lancer des exceptions en cas d'erreur
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+    // Configuration pour récupérer les données sous forme de tableau associatif par défaut
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::ATTR_FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
